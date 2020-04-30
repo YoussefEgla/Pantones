24012bcc4167d05ad18df8f277bf3a2bee1b9505
Youssef AbouEgla <youssefegla@gmail.com>
2020-04-30 10:52:03 +0200

    Pantone Boxes styling

--- src/components/PantoneBox.tsx
+++ src/components/PantoneBox.tsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Paper, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const styles = makeStyles({
   root: {
-    width: "20%",
-    height: "25%",
-    margin: "0 auto",
+    width: "19%",
+    height: "24%",
+    margin: "0.5%",
     cursor: "pointer",
     backgroundColor: (props: PantoneBox) => props.color,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    "&:hover > span": {
+      opacity: 1,
+    },
+  },
+  text: {
+    opacity: "0",
+    transition: "0.5s",
+  },
+  btnContainer: {
+    width: "95%",
+    margin: "0 2.5%",
+    display: "flex",
+    justifyContent: "space-between",
   },
 });
 
 export default function PantoneBox(props: PantoneBox) {
   const classes = styles(props);
-
   return (
-    <div className={classes.root}>
-      <div>PantoneBox</div>
+    <Paper className={classes.root} elevation={0}>
       <div></div>
-    </div>
+      <Typography className={classes.text} variant="overline">
+        COPY
+      </Typography>
+      <div className={classes.btnContainer}>
+        <Typography variant="button">{props.name}</Typography>
+        <Typography variant="button">Shades</Typography>
+      </div>
+    </Paper>
   );
 }
