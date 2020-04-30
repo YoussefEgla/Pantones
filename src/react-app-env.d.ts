27bca2560459867c517af9985c884fc9acc77e9e
Youssef AbouEgla <youssefegla@gmail.com>
2020-04-30 08:46:26 +0200

    pantones collection view with props passed from parent

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="react-scripts" />
+
+// Views
+
+/**
+ * Pantones View
+ */
+interface Pantones {
+  name: string;
+  id: string;
+  emoji?: string;
+  colors: { name: string; color: string }[];
+}
---
+++ src/views/Pantones.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Pantones(props: Pantones) {
+  return (
+    <div>
+      <header>
+        <nav>{/** Navigation things */}</nav>
+      </header>
+      <main>{/** Pantone list */}</main>
+      <footer>{/** Footer Content */}</footer>
+    </div>
+  );
+}
