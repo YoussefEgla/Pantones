d9a5d1807e44a66faff4aee2b2b1b54edca15b95
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-01 06:35:13 +0200

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import collections from "./utils/seedcolors";
 import { collectionWithShades } from "./utils/shades";
 import Pantones from "./views/Pantones";
 
 function App() {
   return (
-    <div className="App">
+    <Switch>
+      <Route exact path="/" render={() => <h1>ALL COLLECTIONS VIEW</h1>} />
+      <Route
+        exact
+        path="/collection/:id"
+        render={() => <h1>COLLECTION VIEW</h1>}
+      />
+      {/* <div className="App">
       <Pantones {...collectionWithShades(collections[0])} />
-    </div>
+    </div> */}
+    </Switch>
   );
 }
 
