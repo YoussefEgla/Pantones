3c59654102fe12a507714f02cc36d2c9659aa650
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-01 08:36:18 +0200

    styling minicollection

--- src/components/MiniCollection.tsx
+++ src/components/MiniCollection.tsx
@@ -1,20 +1,34 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles({
   root: {
+    width: "225px",
+    height: "250px",
+    margin: "25px 0",
     backgroundColor: "White",
     border: "1px solid black",
     borderRadius: "5px",
     padding: "0.5rem",
-    position: "relative",
-    overflow: "hidden",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
     "&:hover": {
       cursor: "pointer",
     },
   },
-  section: {
-    backgroundColor: "red",
+  container: {
+    width: "100%",
+    height: "75%",
+    backgroundColor: "gray",
+  },
+  title: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
+    textDecoration: "none",
+    color: "black",
   },
 });
 
@@ -22,10 +36,11 @@ export default function MiniCollection(props: Pantones) {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <h1>Mini Collection</h1>
-      <section className={classes.section}>
-        <h1>Mini Pantones</h1>
-      </section>
+      <div className={classes.container}></div>
+      <Typography variant="button" className={classes.title}>
+        <span>{props.name}</span>
+        <span>{props.emoji}</span>
+      </Typography>
     </div>
   );
 }
