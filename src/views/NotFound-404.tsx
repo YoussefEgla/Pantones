001fd4a07d84d289e8157eeafcccfb21d2215e91
Youssef AbouEgla <youssefegla@gmail.com>
2020-05-04 03:37:54 +0200

    styling

--- src/views/NotFound-404.tsx
+++ src/views/NotFound-404.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    width: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default function NotFound() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>404 - You hit the end of the road</h1>
+    <div className={classes.root}>
+      <Typography variant="h1">404 - You hit the end of the routes</Typography>
     </div>
   );
 }
