fbf23ff9aece7c1f22228eaebc4b52775625d4c4
Youssef AbouEgla <youssefegla@gmail.com>
2020-10-22 01:48:55 +0200

    Added GitHub link

--- src/views/Collections.tsx
+++ src/views/Collections.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Typography, Button } from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 import { collectionsStyles as useStyles } from "./styles";
 import { CollectionCard } from "../components";
@@ -19,8 +20,11 @@ export default function Collections(props: Collections) {
       <header className={classes.header}>
         <div className={classes.nav}>
           <Typography variant="h5" component="p">
-            Pantones
+            Pantones Organizer
           </Typography>
+          <a href="https://github.com/YoussefEgla/Pantones">
+            <GitHubIcon style={{ color: "white" }} />
+          </a>
           <Link to="/collection/new">
             <Button variant="contained" color="secondary">
               Create Collection
